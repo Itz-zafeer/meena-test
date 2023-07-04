476351d1e28cdd65a6b143fd7c6a439178bd50c9
MudassirAhmed0 <mudassir0ahmed1@gmail.com>
2023-07-04 19:42:53 +0500

    origin

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,22 +1,20 @@
-
-// AOS 
+// AOS
 AOS.init({ once: true });
 
 const handleLang = (selected) => {
-    selected.parentElement.classList.add("active");
-    setTimeout(() => {
-        if (window.location.href.includes("/ar/")) {
-            window.location.href = window.location.href.replace("/ar/", "/");
-          } else {
-            window.location.href =
-              window.location.origin + "/ar" + window.location.pathname;
-          }
-    }, 800);
-  };
+  selected.parentElement.classList.add("active");
+  setTimeout(() => {
+    if (window.location.href.includes("/ar/")) {
+      window.location.href = window.location.href.replace("/ar/", "/");
+    } else {
+      window.location.href =
+        window.location.origin + "/ar" + window.location.pathname;
+    }
+  }, 800);
+};
 
-
-  // HAMBURGER 
-  var menu = document.querySelector(".menu")
+// HAMBURGER
+var menu = document.querySelector(".menu");
 var spantwo = document.querySelector(".line-2");
 var spanone = document.querySelector(".line-1");
 var spanthree = document.querySelector(".line-3");
@@ -30,43 +28,70 @@ menu.onclick = () => {
   sidebar.classList.toggle("active");
   body.classList.toggle("active");
 };
-const faqEl = document.querySelectorAll(".faq")
+const faqEl = document.querySelectorAll(".faq");
 
-faqEl.forEach((faq)=>{
-  faq.addEventListener("click",()=>{
-  const selectedFaq = faq 
-  setTimeout(() => {
-    const yOffset  = -(window.innerWidth / 100 * 4.97135416667)
-  const y = selectedFaq.getBoundingClientRect().top + window.pageYOffset + yOffset;
-  window.scrollTo({top: y, behavior: 'smooth'});
-  }, 800);
-  
-  faqEl.forEach((v)=>{
-   const otherFaq = v
-   if(otherFaq === selectedFaq){
-    otherFaq.classList.toggle("active")
-   }else{
-    otherFaq.classList.remove("active")
-   }
-  }) 
-  })
-})
+faqEl.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    const selectedFaq = faq;
+    const isActive = selectedFaq.classList.contains("active");
 
-const allInputEl = document.querySelectorAll(["input","textarea"]) 
-const handelForm = (e)=>{
-  e.preventDefault()  
-  
-  allInputEl.forEach((allInput)=>{
-    if(!allInput.value){
-  allInput.parentElement.parentElement.classList.add("error")
-
-setTimeout(()=>{
-  allInput.parentElement.parentElement.classList.remove("error")
-
-},8000)
+    faqEl.forEach((v) => {
+      const otherFaq = v;
+      if (otherFaq === selectedFaq) {
+        otherFaq.classList.toggle("active");
+      } else {
+        otherFaq.classList.remove("active");
+      }
+    });
+    if (!isActive) {
+      setTimeout(() => {
+        const yOffset =
+          window.innerWidth > 768
+            ? -((window.innerWidth / 100) * 4.97135416667)
+            : -65;
+        const y =
+          selectedFaq.getBoundingClientRect().top +
+          window.pageYOffset +
+          yOffset;
+        window.scrollTo({ top: y, behavior: "smooth" });
+      }, 800);
     }
-  })
-  
-}
+  });
+});
 
-document.querySelector("form")?.addEventListener("submit",handelForm)
+const inputElements = document.querySelectorAll(["input", "textarea"]);
+const handleForm = (e) => {
+  e.preventDefault();
+  firstErrorElement = null;
+  inputElements.forEach((inputElement) => {
+    if (!inputElement.value) {
+      if (!firstErrorElement) {
+        firstErrorElement = inputElement;
+      }
+      inputElement.parentElement.parentElement.classList.add("error");
+
+      setTimeout(() => {
+        inputElement.parentElement.parentElement.classList.remove("error");
+      }, 8000);
+    }
+  });
+  if (firstErrorElement) {
+    const yOffset =
+      window.innerWidth > 768
+        ? -((window.innerWidth / 100) * 4.97135416667)
+        : -65;
+    // const y =
+    //   firstErrorElement.getBoundingClientRect().top +
+    //   window.pageYOffset +
+    //   yOffset;
+    // window.scrollTo({ top: y, behavior: "smooth" });
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $(firstErrorElement).offset().top + yOffset,
+      },
+      800
+    );
+  }
+};
+
+document.querySelector("form")?.addEventListener("submit", handleForm);
--- assets/javascripts/scrollprogress.js
+++ assets/javascripts/scrollprogress.js
@@ -3,123 +3,170 @@ function isElementInViewport(element) {
   return (
     rect.top >= 0 &&
     rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
   );
-} 
+}
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    
-    let offset = window.location.pathname == "/" || window.location.pathname == "/index.html" ?  window.innerHeight + 100:100;
-    let circleContainer = document.querySelector(".circle-container");
-    let circlePath = document.querySelector('.circle-container path');
-    let pathLength = circlePath.getTotalLength();
-    circlePath.style.transition =           circlePath.style.WebkitTransition = 'none';
-    circlePath.style.strokeDasharray = pathLength;
-    circlePath.style.strokeDashoffset = pathLength;
-    circlePath.getBoundingClientRect();
-    circlePath.style.transition = circlePath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+document.addEventListener("DOMContentLoaded", function (event) {
+  let offset =
+    window.location.pathname == "/" || window.location.pathname == "/index.html"
+      ? window.innerHeight + 100
+      : 100;
+  let circleContainer = document.querySelector(".circle-container");
+  let circlePath = document.querySelector(".circle-container path");
+  let pathLength = circlePath.getTotalLength();
+  circlePath.style.transition = circlePath.style.WebkitTransition = "none";
+  circlePath.style.strokeDasharray = pathLength;
+  circlePath.style.strokeDashoffset = pathLength;
+  circlePath.getBoundingClientRect();
+  circlePath.style.transition = circlePath.style.WebkitTransition =
+    "stroke-dashoffset 10ms linear";
 
-    let updateLoader = () => {
+  let updateLoader = () => {
+    let scrollTop = window.scrollY;
+    let docHeight = document.body.offsetHeight;
+    let winHeight = window.innerHeight;
+    let height = docHeight - winHeight;
+    let progress = pathLength - (scrollTop * pathLength) / height;
+    circlePath.style.strokeDashoffset = progress;
 
-      let scrollTop = window.scrollY;
-      let docHeight = document.body.offsetHeight;
-      let winHeight = window.innerHeight;
-      let height = docHeight - winHeight;
-      let progress = pathLength - (scrollTop * pathLength / height);
-      circlePath.style.strokeDashoffset = progress;
-
-      if (scrollTop > offset) {
-        circleContainer.classList.add("active");
-        if((window.location.pathname == "/" || window.location.pathname == "/index.html")){
-          document.querySelector('.cta').style.opacity= 1
-        }
-        document.querySelector('header').classList.add('stickyHeader')
-      } else {
-        circleContainer.classList.remove("active");
-        if((window.location.pathname == "/" || window.location.pathname == "/index.html")){
-          document.querySelector('.cta').style.opacity= 0
-        }
-        document.querySelector('header').classList.remove('stickyHeader')
-
-
+    if (scrollTop > offset) {
+      circleContainer.classList.add("active");
+      if (
+        window.location.pathname == "/" ||
+        window.location.pathname == "/index.html"
+      ) {
+        document.querySelector(".cta").style.opacity = 1;
       }
-            
+      document.querySelector("header").classList.add("stickyHeader");
+    } else {
+      circleContainer.classList.remove("active");
+      if (
+        window.location.pathname == "/" ||
+        window.location.pathname == "/index.html"
+      ) {
+        document.querySelector(".cta").style.opacity = 0;
+      }
+      document.querySelector("header").classList.remove("stickyHeader");
     }
+  };
 
-  circleContainer.onclick = function(){
-    window.scrollTo({ top: 0, behavior: 'smooth'});
-  }
+  circleContainer.onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
-  const animation2 = document.getElementById('lineanimation2')
+  const animation2 = document.getElementById("lineanimation2");
   const heartPath = document.getElementById("heartLine");
-  const animation3 = document.getElementById('lineanimation3')
+  const animation3 = document.getElementById("lineanimation3");
   const heartPath3 = document.getElementById("heartLine3");
-  if(heartPath){
-    heartPath.parentElement.style.opacity =0
+  if (heartPath) {
+    heartPath.parentElement.style.opacity = 0;
   }
-  if(heartPath3){
-    heartPath3.parentElement.style.opacity =0
+  if (heartPath3) {
+    heartPath3.parentElement.style.opacity = 0;
   }
-  let isFirstVisitAnimation2 = false 
-  let isFirstVisitAnimation3 = false 
-  window.onscroll = () => {
-    updateLoader();
-    if(heartPath) {
-    if(isElementInViewport(animation2)){
-      if(!isFirstVisitAnimation2){  
-heartPath.parentElement.style.opacity =1
+  let isFirstVisitAnimation2 = false;
+  let isFirstVisitAnimation3 = false;
+
+  if (heartPath) {
+    if (isElementInViewport(animation2)) {
+      if (!isFirstVisitAnimation2) {
+        heartPath.parentElement.style.opacity = 1;
         const pathSize = heartPath.getTotalLength();
-        console.log(pathSize)
+        console.log(pathSize);
         heartPath.style.strokeDasharray = `${pathSize}px`;
         heartPath.style.strokeDashoffset = `${pathSize}px`;
         heartPath.style.animation = "line-anime 3s ease-in forwards";
         heartPath.style.animationDelay = `${0 * 0.3}s`;
-        isFirstVisitAnimation2 = true 
-
+        isFirstVisitAnimation2 = true;
       }
       // paths.forEach((path, index) => {
       // });
-    }}
-    if(heartPath3) {
-    if(isElementInViewport(animation3)){
-      if(!isFirstVisitAnimation3){  
-heartPath3.parentElement.style.opacity =1
-        const pathSize = heartPath3.getTotalLength(); 
-        heartPath3.style.strokeDasharray = `${pathSize}px`;
-        heartPath3.style.strokeDashoffset = `${pathSize}px`;
-        heartPath3.style.animation = "line-anime 3s ease-in forwards";
-        heartPath3.style.animationDelay = `${0 * 0.3}s`;
-        isFirstVisitAnimation2 = true 
-
-      }
-      // paths.forEach((path, index) => {
-      // });
-    }}
+    }
   }
 
-  updateLoader();
+  window.onscroll = () => {
+    updateLoader();
+    if (heartPath) {
+      if (isElementInViewport(animation2)) {
+        if (!isFirstVisitAnimation2) {
+          heartPath.parentElement.style.opacity = 1;
+          const pathSize = heartPath.getTotalLength();
+          console.log(pathSize);
+          heartPath.style.strokeDasharray = `${pathSize}px`;
+          heartPath.style.strokeDashoffset = `${pathSize}px`;
+          heartPath.style.animation = "line-anime 3s ease-in forwards";
+          heartPath.style.animationDelay = `${0 * 0.3}s`;
+          isFirstVisitAnimation2 = true;
+        }
+        // paths.forEach((path, index) => {
+        // });
+      }
+    }
+    if (heartPath3) {
+      if (isElementInViewport(animation3)) {
+        if (!isFirstVisitAnimation3) {
+          heartPath3.parentElement.style.opacity = 1;
+          const pathSize = heartPath3.getTotalLength();
+          heartPath3.style.strokeDasharray = `${pathSize}px`;
+          heartPath3.style.strokeDashoffset = `${pathSize}px`;
+          heartPath3.style.animation = "line-anime 3s ease-in forwards";
+          heartPath3.style.animationDelay = `${0 * 0.3}s`;
+          isFirstVisitAnimation2 = true;
+        }
+        // paths.forEach((path, index) => {
+        // });
+      }
+    }
+  };
 
+  updateLoader();
 });
-if(!(window.location.pathname == "/" || window.location.pathname == "/index.html")){
-  document.querySelector('.cta').style.opacity= 1
+if (
+  !(
+    window.location.pathname == "/" || window.location.pathname == "/index.html"
+  )
+) {
+  document.querySelector(".cta").style.opacity = 1;
 }
-document.querySelectorAll('.heroLine').forEach((heroLine,index)=>{
-  heroLine.style.opacity = 1
-  const path  = heroLine.querySelector('path')
+
+document.querySelectorAll(".heroLine").forEach((heroLine, index) => {
+  heroLine.style.opacity = 1;
+  const path = heroLine.querySelector("path");
   const pathSize = path.getTotalLength();
-  
+
   path.style.strokeDasharray = `${pathSize}px`;
   path.style.strokeDashoffset = `${pathSize}px`;
   path.style.animation = "line-anime 1s ease forwards";
-  path.style.animationDelay = `${index * 0.5}s`; 
-})
+  path.style.animationDelay = `${index * 0.5}s`;
+});
 
 // document.querySelectorAll('.asterisk path').forEach((path,index)=>{
 //   const pathSize = path.getTotalLength();
-  
+
 //   path.style.strokeDasharray = `${pathSize}px`;
 //   path.style.strokeDashoffset = `${pathSize}px`;
 //   path.style.animation = "line-anime 0.5s ease forwards";
-//   path.style.animationDelay = `${index * 0.5}s`; 
+//   path.style.animationDelay = `${index * 0.5}s`;
 // })
+
+document.querySelectorAll("#arrowAnimation path").forEach((path, index) => {
+  if (index == 0) {
+    const pathSize = path.getTotalLength();
+
+    path.style.strokeDasharray = `${pathSize}px`;
+    path.style.strokeDashoffset = `${pathSize}px`;
+    path.style.animation = "line-anime 1s ease forwards";
+    path.style.animationDelay = `0s`;
+  } else {
+    console.log(index);
+    const pathSize = path.getTotalLength();
+    path.style.strokeDasharray = `${pathSize}px`;
+    path.style.strokeDashoffset = `${pathSize}px`;
+    // path.style.animation = " line-anime 0.5s ease forwards";
+    path.style.animation =
+      " 0.5s ease-in 0.7s 1 normal forwards running line-anime";
+  }
+});
